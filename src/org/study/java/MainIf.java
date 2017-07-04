8c552ba429eaa4122683ae6b2087c4067b6ccf26
britz255@gmail.com <!qmfflcm255>
2017-07-04 22:14:40 +0900

    ADD: if, for 예제 추가

--- src/org/study/java/MainIf.java
+++ src/org/study/java/MainIf.java
@@ -6,6 +6,45 @@ package org.study.java;
 public class MainIf {
     public static void run() {
 
+        if( 1 == 1) {
+            System.out.println("같다");
+        } else {
+            System.out.println("다르다");
+        }
+
+        // 1. 점수가 90점 이상이면 A, 80점 이상이면 B, 70점 이상이면 C, 미만이면 D를 출력하는 조건문을 만들어야 한다.
+        // 1-1. 점수가 98점이면 A+를 출력하도록 추가한다.
+
+        // 2. 입력받은 값이 아래와 같은 조건일 때 출력하는 조건문을 만들어야 한다.
+        // 홀수만 출력하기
+        // 짝수만 출력하기
+        // 3의 배수만 출력하기
+        // 5의 배수만 출력하기
+        int num = 2;
+        if (num % 2 == 1) {
+            System.out.println(num);
+        }
+        if (num % 2 == 0) {
+            System.out.println(num);
+        }
+        if (num % 3 == 0) {
+            System.out.println(num);
+        }
+        if (num % 5 == 0) {
+            System.out.println(num);
+        }
+
+        // 3. 성이 "이"씨면 "이가"를 출력하고, 김씨면 "김가"를 출력하고, 오씨면 "오가"를 출력하라.
+        String name = "이승엽";
+        if(name.startsWith("이")) {
+            System.out.println("이가");
+        } else if(name.startsWith("김")) {
+            System.out.println("김가");
+        } else if(name.startsWith("오")) {
+            System.out.println("오가");
+        } else {
+            System.out.println("??가");
+        }
 
     }
 }
--- src/org/study/java/Mainfor.java
+++ src/org/study/java/Mainfor.java
@@ -4,4 +4,35 @@ package org.study.java;
  * Created by YS on 2017-07-04.
  */
 public class Mainfor {
+    public static void run() {
+
+        for(int num=0; num <= 20; num++) {
+            if (num % 2 == 1) {
+                System.out.println(num +"은(는) 홀수");
+            }
+            if (num % 2 == 0) {
+                System.out.println(num +"은(는) 짝수");
+            }
+            if (num % 3 == 0) {
+                System.out.println(num +"은(는) 3의 배수");
+            }
+            if (num % 5 == 0) {
+                System.out.println(num +"은(는) 5의 배수");
+            }
+        }
+
+        // 무한루프 예제
+        for(int i=0; true; i++){
+            System.out.println(i);
+
+            if(i > 32678) {
+                break;
+            }
+        }
+
+        // 홀수만 더하기
+        // 짝수만 더하기
+        // 2의 배수만 더하기
+        // 3의 배수만 더하기
+    }
 }
